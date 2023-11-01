a562c48183346329b2e7b1cdcb607b56ceabe7dc
germanarq <ger_arquati@yahoo.com.ar>
2023-11-01 11:39:08 -0300

    Update_v1.0

---
+++ index-tpi.js
@@ -0,0 +1,9 @@
+function mostrarformOradores() {
+    document.getElementById('formularioOradores').style.display = 'block';
+    document.getElementById('gracias').style.display ='none';
+}
+
+function ocultarformOradores(){
+    document.getElementById('formularioOradores').style.display = 'none';
+    document.getElementById('gracias').style.display ='block';
+}
